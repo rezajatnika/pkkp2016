51d51cbc6f6531e0ada8fa0dcba15ee0089d1a68
Reza Jatnika <rezajatnika@gmail.com>
2016-08-03 10:05:53 +0700

    Recap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     puts @user.errors.full_messages
-    # if verify_recaptcha(model: @user) && @user.save
-    if @user.save
+    if verify_recaptcha(model: @user) && @user.save
       session[:user_id] = @user.id
       redirect_to registered_path
     else
