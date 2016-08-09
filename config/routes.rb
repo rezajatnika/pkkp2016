eba80fe5648521f88bdac869c77877862dab2db5
Reza Jatnika <rezajatnika@gmail.com>
2016-08-09 12:36:46 +0700

    Add prodi to CSV

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Admin
   namespace :admin do
     root to: 'users#index'
-    resources :users, :majors
+    resources :users, :majors, :posts
   end
 
   # Root
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801121753) do
+ActiveRecord::Schema.define(version: 20160808032929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "majors", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
