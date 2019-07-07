14feaa9a80a441912340374c217a3b7fdd537c50
Reza Jatnika <rezajatnika@gmail.com>
2019-07-07 14:36:47 +0700

    Update 2019

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :participants, only: [:new, :create]
 
-  # get 'panitia', to: 'committees#new'
-  # get 'panitia/reg', to: 'committees#registered', as: 'registered'
+  get 'panitia', to: 'committees#new'
+  get 'panitia/reg', to: 'committees#registered', as: 'registered'
 
-  # get 'peserta', to: 'participants#new'
-  # get 'peserta/reg', to: 'participants#registered'
+  get 'peserta', to: 'participants#new'
+  get 'peserta/reg', to: 'participants#registered'
 end
